2bd11b9aca6a565ec1d45f67d50410db20581eb6
Sainfuw <sainfuw@aol.com>
2018-10-14 23:53:50 -0300

    exercise 47

---
+++ app/assets/javascripts/colors.js
@@ -0,0 +1,62 @@
+var red = 30;
+var green = 30;
+var blue = 30;
+
+var setColors = function() {
+  color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  $('#colorRGB')[0].innerHTML = color
+  $('#box')[0].style.backgroundColor = color;
+}
+
+$(document).on('turbolinks:load', function() {
+  setColors();
+  $('.knob').knob({
+    'min': 0,
+    'max': 255,
+    'lineCap': 'round',
+    change: function(v) {
+      if (this.$[0].dataset.fgcolor == 'red') {
+        red = parseInt(v);
+      } else if (this.$[0].dataset.fgcolor == 'green') {
+        green = parseInt(v);
+      } else {
+        blue = parseInt(v);
+      }
+      setColors();
+    }
+  });
+});
+
+
+// $(document).on('turbolinks:load', function () {
+//   $('#red').knob({
+//     'min': 0,
+//     'max': 255,
+//     'lineCap': 'round',
+//     'fgColor': 'red',
+//     change: function (v) {
+//       red = parseInt(v);
+//       setColors();
+//     }
+//   });
+//   $('#green').knob({
+//     'min': 0,
+//     'max': 255,
+//     'lineCap': 'round',
+//     'fgColor': 'green',
+//     change: function (v) {
+//       green = parseInt(v);
+//       setColors();
+//     }
+//   });
+//   $('#blue').knob({
+//     'min': 0,
+//     'max': 255,
+//     'lineCap': 'round',
+//     'fgColor': 'blue',
+//     change: function (v) {
+//       blue = parseInt(v);
+//       setColors();
+//     }
+//   });
+// });
